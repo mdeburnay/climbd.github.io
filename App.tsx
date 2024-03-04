a37cee2e88e10a3ac137af936b9ebc5638a4f2c7
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2024-03-04 19:35:31 +1100

    basic home page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to</Text>
+      <TextInput placeholder="Distance" />
+      <TextInput placeholder="Incline %" />
+      <TextInput placeholder="Metres Climbed" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#FFF",
   },
 });
